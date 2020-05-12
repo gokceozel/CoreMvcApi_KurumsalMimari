6a8ed6daedd9fe3ca5303ea9fefab61454800a10
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-13 02:59:19 +0300

    start

---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NorthwindKurumsalMimari.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NorthwindKurumsalMimari.DataAccess
+{
+    public class Class1
+    {
+    }
+}
