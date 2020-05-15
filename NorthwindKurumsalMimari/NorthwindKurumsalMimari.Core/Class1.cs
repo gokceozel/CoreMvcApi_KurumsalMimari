17ca6e131bfe95dd766bb79091f1099fe3db3ef6
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-15 03:41:20 +0300

    ...

--- NorthwindKurumsalMimari/NorthwindKurumsalMimari.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NorthwindKurumsalMimari.Core
-{
-    public class Class1
-    {
-    }
-}
--- NorthwindKurumsalMimari/NorthwindKurumsalMimari.Entities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NorthwindKurumsalMimari.Entities
-{
-    public class Class1
-    {
-    }
-}
