2900f0787f43e0940a1bcd857584234deddcaace
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-15 22:50:05 +0300

    .

---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindKurumsalMimari.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindKurumsalMimari.DataAccess.Concrete.EntityFramework.Contexts
+{
+   public class NorthwindContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -0,0 +1,14 @@
+﻿using NorthwindKurumsalMimari.Core.DataAccess.EntityFramework;
+using NorthwindKurumsalMimari.DataAccess.Abstarct;
+using NorthwindKurumsalMimari.DataAccess.Concrete.EntityFramework.Contexts;
+using NorthwindKurumsalMimari.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindKurumsalMimari.DataAccess.Concrete.EntityFramework
+{
+   public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    {
+    }
+}
