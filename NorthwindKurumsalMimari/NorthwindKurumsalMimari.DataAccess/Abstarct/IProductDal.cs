705ac3c0e1ffa41b678af4c289c1cb5893077966
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-15 21:42:18 +0300

    .

---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.DataAccess/Abstarct/IProductDal.cs
@@ -0,0 +1,15 @@
+﻿using NorthwindKurumsalMimari.Core.DataAccess;
+using NorthwindKurumsalMimari.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindKurumsalMimari.DataAccess.Abstarct
+{
+  public  interface IProductDal : IEntityRepository<Product>
+    {
+
+
+
+    }
+}
--- NorthwindKurumsalMimari/NorthwindKurumsalMimari.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NorthwindKurumsalMimari.DataAccess
-{
-    public class Class1
-    {
-    }
-}
