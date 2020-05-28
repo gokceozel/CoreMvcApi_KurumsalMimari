f6fa1021db1d83848d92ee5f708bd5856c1090dc
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-29 02:16:04 +0300

    .

---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,18 @@
+﻿using NorthwindKurumsalMimari.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindKurumsalMimari.Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            throw new NotImplementedException();
+
+        }
+
+       //Security
+    }
+}
