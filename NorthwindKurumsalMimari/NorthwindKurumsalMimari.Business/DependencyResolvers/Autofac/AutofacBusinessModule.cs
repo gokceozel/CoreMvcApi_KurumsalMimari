e9a44fff778c248cf45f594be39190d39763d4b0
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-28 03:02:06 +0300

    .

---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using NorthwindKurumsalMimari.Business.Abstract;
+using NorthwindKurumsalMimari.Business.Concrete;
+using NorthwindKurumsalMimari.DataAccess.Abstarct;
+using NorthwindKurumsalMimari.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindKurumsalMimari.Business.DependencyResolvers.Autofac
+{
+   public class AutofacBusinessModule :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
