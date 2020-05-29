661204408ea3ffd1eb8842c02a991ea46628d9f7
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-05-29 22:20:28 +0300

    token

---
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.Core/Utilities/Security/Encyption/SingingCredentialHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NorthwindKurumsalMimari.Core.Utilities.Security.Encyption
+{
+   public class SingingCredentialHelper
+    {
+        public static SigningCredentials CreateSingingCredentials(SecurityKey securityKey)
+        {
+            return new SigningCredentials(securityKey,algorithm:SecurityAlgorithms.HmacSha256Signature);
+        }
+    }
+}
--- NorthwindKurumsalMimari/NorthwindKurumsalMimari.Core/Utilities/Security/Jwt/JwtHelper.cs
+++ NorthwindKurumsalMimari/NorthwindKurumsalMimari.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -4,7 +4,11 @@ using NorthwindKurumsalMimari.Core.Entities.Concrete;
 using NorthwindKurumsalMimari.Core.Utilities.Security.Encyption;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
+using NorthwindKurumsalMimari.Core.Extensions;
+using System.Linq;
 
 namespace NorthwindKurumsalMimari.Core.Utilities.Security.Jwt
 {
@@ -12,20 +16,53 @@ namespace NorthwindKurumsalMimari.Core.Utilities.Security.Jwt
     {
         public IConfiguration Configuration { get; }
         private TokenOptions _tokenOptions;
+        private DateTime _accessTokenExpiration;
         public JwtHelper(IConfiguration configuration)
         {
             Configuration = configuration;
             _tokenOptions = Configuration.GetSection(key: "TokenOptions").Get<TokenOptions>();
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
         }
 
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
 
             var securityKey = SecurityKeyHelper.SecurityKey(_tokenOptions.SecurityKey);
-            return new AccessToken();
+            var singinCredentials = SingingCredentialHelper.CreateSingingCredentials(securityKey);
+            var jwt = CreateJwtSecurityToken(_tokenOptions, user, singinCredentials, operationClaims);
+            var jwtSecurityTokenHelper = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHelper.WriteToken(jwt);
+            return new AccessToken { 
+            Token=token,
+            Expiration=_accessTokenExpiration
+            };
 
         }
 
+        public JwtSecurityToken CreateJwtSecurityToken(TokenOptions tokenOptions,User user,SigningCredentials signingCredentials,List<OperationClaim> operationClaims)
+        {
+            var jwt = new JwtSecurityToken(
+                 issuer:tokenOptions.Issuer,
+                 audience:tokenOptions.Audience,
+                 expires: _accessTokenExpiration,
+                 notBefore:DateTime.Now,
+                 claims: SetClaims(user,operationClaims),
+                 signingCredentials:signingCredentials
+                );
+
+            return jwt;
+
+        }
+
+        private IEnumerable<Claim> SetClaims(User user,List<OperationClaim> operationClaims)
+        {
+            var claim = new List<Claim>();
+            claim.AddNameIdentityfier(user.Id.ToString());
+            claim.AddEmail(user.Email);
+            claim.AddName($"{user.FirstName}{user.LastName}");
+            claim.AddRoles(operationClaims.Select(x=>x.Name).ToArray());
+            return claim;
+        }
      
     }
 }
